6240bb680c89077ef31debb9e54cd00f751c7486
akhilnaik <akhilnaik.aries@gmail.com>
2015-06-10 12:59:53 -0500

    Update unique.js

--- unique.js
+++ unique.js
@@ -1,37 +1,36 @@
-
-var Unique = function(select_class,no_select_value){
+function Unique(select_class,no_select_value){
 	this.select_class = select_class;//select class is the class of the drop down lists(<select> tags) that have to be exclusive
 	this.no_select_value = no_select_value;//value that is present in the drop down list by default
 	this.list_values = Array.apply(null, new Array($(this.select_class).length)).map(String.prototype.valueOf,this.no_select_value);
 	
 	var _self = this;
-	$(_self.select_class).change(function(){
+	$(select_class).change(function(){
 		var prev_val = _self.list_values[$(_self.select_class).index($(this))];
 		_self.list_values[$(_self.select_class).index($(this))] = $(this).val();
+		console.log("here");
 		_self.enableoption(_self.select_class,prev_val);
-		_self.removeoption(_self.select_class,$(this).val());
+		console.log("here1");
+		_self.removeoption(_self.select_class,$(this).val(),$(_self.select_class).index($(this)));
+		console.log("here2");
 	});
 }
 
-Unique.prototype.removeoption = function(classname,value){
-	if(value!=this.no_select_value)return;
+Unique.prototype.removeoption = function(classname,value,index){
+	console.log("in remove option");
+	if(value==this.no_select_value)return;
 	var arr = $(classname);
 	for(var i=0;i<arr.length;i++){
-		$(arr[i]).children('option[value="'+value+'"]').attr('disabled','disabled');
+		if(i==index)continue;
+		$(arr[i]).children('option[value="'+value+'"]').attr('disabled',true);
 	}
+	
+	//$(arr[index]).children('option[value="'+value+'"]').removeAttr('disabled');
 }
 
 Unique.prototype.enableoption = function(classname,value){
+	console.log("in enable option");
 	var arr = $(classname);
 	for(var i=0;i<arr.length;i++){
-		$(arr[i]).children('option[value="'+value+'"]').attr('disabled',false);
+		console.log($(arr[i]).children('option[value="'+value+'"]').removeAttr('disabled'));
 	}
 }
-
-
-
-
-
-
-
-
