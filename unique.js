1e354b47b26aad6a688841a94ffaefe501106a7e
akhilnaik <akhilnaik.aries@gmail.com>
2015-06-10 13:33:32 -0500

    Update unique.js

--- unique.js
+++ unique.js
@@ -7,30 +7,23 @@ function Unique(select_class,no_select_value){
 	$(select_class).change(function(){
 		var prev_val = _self.list_values[$(_self.select_class).index($(this))];
 		_self.list_values[$(_self.select_class).index($(this))] = $(this).val();
-		console.log("here");
 		_self.enableoption(_self.select_class,prev_val);
-		console.log("here1");
 		_self.removeoption(_self.select_class,$(this).val(),$(_self.select_class).index($(this)));
-		console.log("here2");
 	});
 }
 
 Unique.prototype.removeoption = function(classname,value,index){
-	console.log("in remove option");
 	if(value==this.no_select_value)return;
 	var arr = $(classname);
 	for(var i=0;i<arr.length;i++){
 		if(i==index)continue;
 		$(arr[i]).children('option[value="'+value+'"]').attr('disabled',true);
 	}
-	
-	//$(arr[index]).children('option[value="'+value+'"]').removeAttr('disabled');
 }
 
 Unique.prototype.enableoption = function(classname,value){
-	console.log("in enable option");
 	var arr = $(classname);
 	for(var i=0;i<arr.length;i++){
-		console.log($(arr[i]).children('option[value="'+value+'"]').removeAttr('disabled'));
+		$(arr[i]).children('option[value="'+value+'"]').removeAttr('disabled');
 	}
 }
