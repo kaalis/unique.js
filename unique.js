07ed7ef345531850bd50c3e5f94ffe03536f62a6
akhilnaik <akhilnaik.aries@gmail.com>
2015-06-10 10:27:59 -0500

    Update unique.js

--- unique.js
+++ unique.js
@@ -6,16 +6,15 @@ var Unique = function(select_class,no_select_value){
 	
 	var _self = this;
 	$(_self.select_class).change(function(){
-		if($(this).val()!=_self.no_select_value){
-			var prev_val = _self.list_values[$(_self.select_class).index($(this))];
-			_self.list_values[$(_self.select_class).index($(this))] = $(this).val();
-			_self.enableoption(_self.select_class,prev_val);
-			_self.removeoption(_self.select_class,$(this).val());
-		}
+		var prev_val = _self.list_values[$(_self.select_class).index($(this))];
+		_self.list_values[$(_self.select_class).index($(this))] = $(this).val();
+		_self.enableoption(_self.select_class,prev_val);
+		_self.removeoption(_self.select_class,$(this).val());
 	});
 }
 
 Unique.prototype.removeoption = function(classname,value){
+	if(value!=this.no_select_value)return;
 	var arr = $(classname);
 	for(var i=0;i<arr.length;i++){
 		$(arr[i]).children('option[value="'+value+'"]').attr('disabled','disabled');
