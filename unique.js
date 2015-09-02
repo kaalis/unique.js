ea0783188233baf93dfedd5ab860f6ebfd4a5c23
kaalis <valmiera-9@inbox.lv>
2015-09-02 23:54:05 +0300

    Update unique.js
    Disabling options that are already selected in other selects when they are generated

--- unique.js
+++ unique.js
@@ -1,10 +1,21 @@
+//Source:  https://github.com/akhilnaik/unique.js
+//Example: http://stackoverflow.com/a/30760650
+
 function Unique(select_class,no_select_value){
 	this.select_class = select_class;//select class is the class of the drop down lists(<select> tags) that have to be exclusive
 	this.no_select_value = no_select_value;//value that is present in the drop down list by default
-	this.list_values = Array.apply(null, new Array($(this.select_class).length)).map(String.prototype.valueOf,this.no_select_value);
-	
+	this.list_values = $(select_class).map(function(){
+		return this.value
+	}).get();
 	var _self = this;
-	$(select_class).change(function(){
+
+	$(select_class).on('click', function() {
+		$.each( _self.list_values, function( key, value ) {
+			_self.removeoption(_self.select_class, value, key);
+		});
+	});
+
+	$(select_class).on('change', function() {
 		var prev_val = _self.list_values[$(_self.select_class).index($(this))];
 		_self.list_values[$(_self.select_class).index($(this))] = $(this).val();
 		_self.enableoption(_self.select_class,prev_val);
